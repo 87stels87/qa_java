81cf2cc387dc9a9c08819b274f423e3c6b316ceb
Andrey Okatev <okatev87@yandex.ru>
2022-03-10 23:09:21 +0300

    Изолировал класс Lion от класса Feline (добавил Feline feline в конструктор Lion) + первый тест на класс Lion

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,2 +1,17 @@
+import com.example.Cat;
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 public class LionTest {
+
+    @Test
+    public void testN1t() throws Exception {
+        Lion l1 = new Lion("Самка",new Feline());
+        l1.doesHaveMane();
+        assertEquals(false,  l1.doesHaveMane());
+    }
 }
